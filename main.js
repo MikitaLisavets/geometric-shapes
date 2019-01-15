7ce4fe90a90a2b4adce6b03dcf073f2a35778413
Mikita Lisavets <mikitalisavets@gmail.com>
2019-01-15 12:27:07 +0300

    update

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ const pointColor = '#ff0000'
 
 updateCanvasDimentions()
 canvas.addEventListener('click', addPoint);
+canvas.addEventListener('click', movePoint);
 
 
 // window.addEventListener('resize', updateCanvasDimentions)
@@ -19,7 +20,7 @@ class Point {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.color = '#ff0000'
+    this.color = '#ff0000';
   }
 
   draw() {
@@ -29,20 +30,53 @@ class Point {
     ctx.beginPath();
     ctx.arc(this.x, this.y, 10, 2 * Math.PI, 0);
     ctx.lineWidth = 1;
+    ctx.stroke();
+  }
+}
+
+class Parallelogram {
+  constructor(points) {
+    this.points = points;
+    this.color = '#0000ff';
+    this.center = {
+      x: (this.points[0].x + this.points[2].x) / 2,
+      y: (this.points[0].y + this.points[2].y) / 2
+    }
+  }
+
+  draw() {
+    const lastPoint = {
+      x: this.center.x - (this.points[1].x - this.center.x),
+      y: this.center.y - (this.points[1].y - this.center.y)
+    }
+
     ctx.strokeStyle = this.color;
-    ctx.stroke()
-    canvas.removeEventListener('click', addPoint);
+    ctx.beginPath();
+    ctx.moveTo(this.points[0].x, this.points[0].y);
+    for (let i = 1; i < this.points.length; i++) {
+      ctx.lineTo(this.points[i].x, this.points[i].y);
+    }
+    ctx.lineTo(lastPoint.x, lastPoint.y);
+    ctx.lineTo(this.points[0].x, this.points[0].y);
+    ctx.lineWidth = 1;
+    ctx.stroke();
   }
 }
 
 function addPoint(event) {
-  if (state.points.length < 3) {
-    state.points.push(new Point(event.pageX, event.pageY))
-  }
+  if (state.points.length >= 3) return
+  
+  state.points.push(new Point(event.pageX, event.pageY))
   if (state.points.length === 3) {
-    state.parallelogram = new Parallelogram(event.pageX, event.pageY)
-    state.circle = new Circle(event.pageX, event.pageY)
+    state.parallelogram = new Parallelogram(state.points)
+    // state.circle = new Circle(event.pageX, event.pageY)
   }
+
+  draw();
+}
+
+function movePoint(event) {
+
 }
 
 function updateCanvasDimentions() {
